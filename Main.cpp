ce656b4792d99f35f32e49d2be41a54d413bb252
Cyphersphinx <33814760+Cyphersphinx@users.noreply.github.com>
2017-12-01 04:28:42 -0700

    Main.cpp
    Main file for the Bus Type Class program

---
+++ Main.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+**Write a program that creates a Bus class that includes a bus ID number, 
+number of seats, driver ID number, and current speed. 
+
+**Create a default constructor without any parameters that initializes 
+the value of ID number, number of seats,and driver ID number.  
+(Header file - done / Implementation file -)
+
+**Create another constructor with three parameters of 
+bus id number, number of seats, and driver ID Number. 
+(Header file - done / Implementation file -)
+
+**Car speed should always be initialized as 0.
+
+**The program will also include the member functions to perform the various 
+operations: modify, set, and display bus information 
+(Bus id, number of seats, driver ID number, and current speed).
+
+For example:
+- Set the bus id number (Header file - done / Implementation file -)
+- Set the number of seats (Header file - done / Implementation file -)
+- Set of driver initialized number (Header file - done / Implementation file -)
+----------------------------------------------------
+- Return the bus id number by using get method (Header file -  / Implementation file -)
+- Return the number of seats (Header file -  / Implementation file -)
+- Return of driver ID 
+number (Header file -  / Implementation file -)
+- Return the current speed (Header file -  / Implementation file -)
+-----------------------
+Member function to display the bus information
+- busInformation() (Header file - done / Implementation file -)
+
+Also write two member functions, Accelerate() and Brake(). 
+**Whenever Accelerate() is called,increase the speed by 5 (cannot exceed 50), and  (Header file -  / Implementation file -)
+** whenever Brake() is called, decrease the speed by 5 (cannot be negative).   (Header file -  / Implementation file -)
+**Write a program that declares several Bus objects and demonstrates that all the functions work correctly.  
+
+*******************************************************************************/
+
+// Header declarations
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}  // end of Function: main
+
+
+
