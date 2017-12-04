7fd073ebe870482e4277c15c66d29aa004253ad1
Cyphersphinx <33814760+Cyphersphinx@users.noreply.github.com>
2017-12-03 21:45:12 -0700

    Update busType.h

--- busType.h
+++ busType.h
@@ -1,16 +1,15 @@
-//busClass.h, the specification file for the class busType.
-
+//#pragma once
 class busType
 {
-    public:
-      void setBusID(int busNum); 
+public:
+	void setBusID(int busNum); 
       //Function to set the bus' ID number
       //The busIDNumber is set according to the parameter
       //PostCondition:  busIDNumber = busNum;
       //                The function checks whether the value of busNum is valid,
       //                the default value of 0 is assigned 
       
-      void setNumOfSeats(int seatNum);
+    void setNumOfSeats(int seatNum);
       //Function to set the number of seats on the bus 
       //The numberOfSeats is set according to the parameter
       //PostCondition:  numberOfSeats = seatNum;
@@ -24,25 +23,25 @@ class busType
       //                The function checks whether the value of driverIDNum is valid,
       //                the default value of 0 is assigned 
       
-      int returnBusID();
+      int returnBusID() const;
       //Function: to return the bus ID number 
       //Parameters passed: none
       //PostCondition:  The value of busIDNumber is returned
       //
       
-      int returnNumOfSeats();
+      int returnNumOfSeats() const;
       //Function: to return the number of seats for the bus
       //Parameters passed: none
       //PostCondition:  The value of numberOfSeats is returned
       //
       
-      int returnDriverID();
+      int returnDriverID() const;
       //Function: to return the bus driver's ID number
       //Parameters passed: none
       //PostCondition:  The value of driverID is returned
       //
       
-      int returnCurrentSpeed();
+      int returnCurrentSpeed() const;
       //Function: to return the current speed of the bus
       //Parameters passed: none
       //PostCondition:  The value of busSpeed is returned
@@ -59,14 +58,20 @@ class busType
       //Parameters passed: none
       //PostCondition:  busSpeed -= 5 unless to go below 0 mph
       //
-      
+
+	  void busInformation() const;
+      //Function: To display the bus information
+      //Parameters passed: none
+      //PostCondition:  data for the bus info is displayed 
+      //
+
       busType();
       //Function: To be the 'base' default constructor to initialize all member variables to 0
       //Parameters passed: none
       //PostCondition:  busIDNumber = 0; numberOfSeats = 0; driverID = 0;
       //                busSpeed = 0;
       
-      busType(int busIDnum, int numbOfSeats, int driverIDNum);
+      busType(int busIDNum, int numbOfSeats, int driverIDNum);
       //Function: To be the a constructor that initializes member variables to passed in parameters
       //Parameters passed: busIDNumber, numberOfSeats, and driverID are set according to the parameters
       //PostCondition:  busIDNumber = budIDNum; numberOfSeats = numbOfSeats;
