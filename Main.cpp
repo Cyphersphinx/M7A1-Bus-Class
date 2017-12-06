0f4179131858d1959733328145e79bf47e1f643f
Cyphersphinx <33814760+Cyphersphinx@users.noreply.github.com>
2017-12-05 17:06:55 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -43,34 +43,45 @@ Also write two member functions, Accelerate() and Brake().
 #include "busType.h"
 
 void mainMenuSelection();
-void busMenuSelection();
+void busMenuSelection(busType& busTemp);
 
 using namespace std;
 
 
 int main()
 {
-  	int currentSpeed = 0;
-	int busID = 0;
-	int driver = 0;
-	int seats = 0;  // Max # of seats is set to 33
 	int mainSelect = 0;
 
-
-	busType busOne;
-
+	busType busOne (1551, 30, 43241);
 	busType busTwo (2342, 22, 41350);
-
-	busType busThree;
-	
-	busType busFour;
-
-	busType selectedBus;
+	busType busThree (2324, 15, 21341);
+	busType busFour (23423, 14, 13414);
+    // busType selectedBus;
 
 	while (mainSelect != 9)
 	{
 		mainMenuSelection();
 		cin >> (mainSelect);
+		switch (mainSelect)
+		{
+		    case 1:
+		      busMenuSelection(busOne);
+		      break;
+		    case 2:
+		      busMenuSelection(busTwo);
+		      break;
+		    case 3:
+		      busMenuSelection(busThree);
+		      break;
+		    case 4:
+		      busMenuSelection(busFour);
+		      break;
+		    case 9:
+		      cout << "Good Bye!!";
+		      break;
+		    default:
+		      cout << "You have made an improper request. Please try again\n\n\n\n";
+		}
 	
 	}
 	system("PAUSE");
@@ -92,21 +103,96 @@ void mainMenuSelection()
 	cout << "\nEnter 9 to exit ";
 	cout << "\nPlease enter your selection: ";
 
-}
+}  // end of mainMenuSelection
 
-void busMenuSelection()
+void busMenuSelection(busType& busTemp)
 {
     //Function: To display bus menu options for the user
-    //Parameters passed: none
+    //Parameters passed: # of Bus selected
     //PostCondition:  No parameters changed 
     //
-	cout << "\n\n		Bus Selection Menu";
-	cout << "\n1. Increase Speed:";
-	cout << "\n2. Decerease Speed: ";
-	cout << "\nSet/Change the:";
-	cout << "\n3.              Bus ID #:";
-	cout << "\n4.              Bus Driver ID: ";
-	cout << "\n5.              # of seats on the bus: ";
-	cout << "\nEnter 9 to return to the Main Menu";
-	cout << "\nPlease enter your selection: ";
-}
+    int menuSelect = -1;
+    int currentSpeed = 0;
+	int busID = 0;
+	int driver = 0;
+	int seats = 0;  // Max # of seats is set to 33
+    
+    
+	while (menuSelect != 0)  
+	{
+	    cout << "\n\n		Bus Settings Selection Menu";
+    	cout << "\n1. Increase Speed by 5 mph:";
+	    cout << "\n2. Decerease Speed by 5 mph: ";
+	    cout << "\n  Set/Change the:";
+	    cout << "\n3.              -Bus ID #:";
+	    cout << "\n4.              -Bus Driver ID: ";
+	    cout << "\n5.              -# of occupied seats on the bus: ";
+	    cout << "\n6. Show Bus ID #:";
+	    cout << "\n7. Show Bus Driver ID #:";
+	    cout << "\n8. Show # of occupied seats on the bus:";
+	    cout << "\n9. Show Bus current speed:";
+	    cout << "\nEnter 0 to return to the Main Menu";
+	    cout << "\nPlease enter your selection: ";
+	    cin >> menuSelect;
+	    switch (menuSelect)
+	    {
+	        case 1:  // Increase Speed
+	          cout << "\nBus speed has been set to:";
+	          busTemp.Accelerate();
+	          currentSpeed = busTemp.getCurrentSpeed();
+	          cout << currentSpeed << endl << endl;
+	          break;
+	        case 2:  // Decrease Speed
+	          cout << "\nBus speed has been set to:";
+	          busTemp.Brake();
+	          currentSpeed = busTemp.getCurrentSpeed();
+	          cout << currentSpeed << endl << endl;
+	          break;
+	        case 3:  // Change Bus ID #
+	          cout << "\nThe current ID for this bus is: " << busTemp.getBusID();
+	          cout << "\nWhat is the new ID # for this bus:";
+	          cin >> busID;
+	          busTemp.setBusID(busID);
+	          cout << endl << endl;
+	          break;
+	        case 4:  // Change Bus Driver ID #
+	          cout << "\nThe current Bus Driver ID # for this bus is: " << busTemp.getDriverID();
+	          cout << "\nWhat is the new Driver ID # for this bus:";
+	          cin >> driver;
+	          busTemp.setDriverID(driver);
+	          cout << endl << endl;
+	          break;
+	        case 5:  // Change # occupied seats on bus
+	          cout << "\nPlease enter the new number of seats occupied: ";
+	          cin >> seats;
+	          busTemp.setNumOfSeats(seats);
+	          cout << endl << endl;
+	          break;
+	        case 6:  // Show bus ID # 
+	          cout << "\nThe current ID # for this Bus is: "; 
+	          busID = busTemp.getBusID();
+	          cout << busID <<  endl << endl;
+	          break;
+	        case 7:  // Display Bus Driver ID # 
+	          cout << "\nThe current Driver ID # for this Bus is: "; 
+	          driver = busTemp.getDriverID();
+	          cout << driver << endl << endl;
+	          break;
+	        case 8:  // Display # occupied seats on bus
+	          cout << "\nThe current # of occupied seats for this Bus is: "; 
+	          seats = busTemp.getNumOfSeats();
+	          cout << seats << endl << endl;
+	          break;
+	        case 9:  // Display Bus's current speed
+	          cout << "\nThe current speed for this Bus is: "; 
+	          currentSpeed = busTemp.getCurrentSpeed();
+	          cout << currentSpeed << endl << endl;
+	          break;
+	        case 0:  //Used to exit Bus Selection Menu
+	          break;
+	        default:
+	          cout << "\nYou have entered an incorrect value.  Please try again\n";
+	        
+	    } // end of switch: menuSelect
+	} // end of while: menuSelect
+}  // end of function: busMenuSelection
